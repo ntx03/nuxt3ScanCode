f5ce8c2d037fd2823e9112eb7b8d00506c273ee8
KachurAL <ntx033@yandex.ru>
2022-12-12 16:42:21 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page">
+        <Header />
+    </div>
+</template>
+<style scoped>
+.page {
+    width: 100%;
+    height: 100vh;
+    background-color: antiquewhite;
+}
+</style>
+<script setup>
+const onScan = (decodedText, decodedResult) => {
+    console.log(decodedText);
+    console.log(decodedResult);
+};
+</script>
---
+++ components/scanCode.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Html5QrcodeScanner } from "html5-qrcode";
+
+const props = defineProps({
+    fps: Number,
+    qrbox: Number,
+});
+const config = {
+    fps: props.fps,
+    qrbox: props.qrbox,
+};
+const onScanSuccess = (decodedText, decodedResult) => {
+    const emit = defineEmits();
+    emit("onScan", decodedText, decodedResult);
+};
+const html5QrcodeScanner = new Html5QrcodeScanner("qr-code-full-region", config);
+html5QrcodeScanner.render(onScanSuccess);
+</script>
+
+<style>
+.width {
+    width: 500px;
+}
+</style>
+<template><div id="qr-code-full-region" class="width"></div></template>
